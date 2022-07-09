c96c63b0f5381d461a764caa166fb8ef55a29656
Aswin Soundararajan <aswin.soundararajan@gmail.com>
2022-07-09 03:26:34 +0000

    added custom https for frontdoor

--- modules/frontdoor/frontdoor.tf
+++ modules/frontdoor/frontdoor.tf
@@ -67,4 +67,11 @@ resource "azurerm_frontdoor_custom_https_configuration" "custom_https_configurat
     certificate_source = "FrontDoor"
   }
 }
+resource "azurerm_frontdoor_custom_https_configuration" "custom_https_configuration_domain_apex" {
+  frontend_endpoint_id              = azurerm_frontdoor.portfolio-frontdoor.frontend_endpoints["${var.front_end_point_custom_domain_apex}"]
+  custom_https_provisioning_enabled = true
+  custom_https_configuration {
+    certificate_source = "FrontDoor"
+  }
+}
 
